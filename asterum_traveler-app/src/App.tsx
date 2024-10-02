ea342ca318ed5518ae7b2cfe603552dc5795cae3
Sumin Park <clappingmin@gmail.com>
2024-10-02 22:09:25 +0900

    style: 배경 스타일 초기화

--- asterum_traveler-app/src/App.tsx
+++ asterum_traveler-app/src/App.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div>
+    <Wrapper>
       <Outlet />
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
 
 export default App;
