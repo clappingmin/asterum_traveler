76041a1eda037f9dda024951202c18027e2b8900
Sumin Park <clappingmin@gmail.com>
2025-01-07 03:29:38 +0900

    feat: 제품 데이터 가져오기 연결

--- apps/asterum_traveler-admin/src/shared/services/reportService.ts
+++ apps/asterum_traveler-admin/src/shared/services/reportService.ts
@@ -1,7 +1,8 @@
+import { Product } from './../../../../../packages/types/interfaces/report.interface';
 import { v4 as uuidv4 } from 'uuid';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { db, storage } from '../firebaseConfig';
-import { doc, setDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, query, setDoc } from 'firebase/firestore';
 import { ProductBase } from '@asterum/types';
 
 export const imageUpload = async (image: File) => {
@@ -17,8 +18,9 @@ export const imageUpload = async (image: File) => {
 /**
  *  제품 추가하기
  * @param {ProductBase} prdocut
+ * @return {Promise<string>} product id
  */
-export async function addProduct(prdocut: ProductBase) {
+export async function addProduct(prdocut: ProductBase): Promise<string> {
   try {
     const productId = uuidv4();
 
@@ -26,5 +28,30 @@ export async function addProduct(prdocut: ProductBase) {
     return productId;
   } catch (e) {
     throw e;
+  }
+}
+
+/**
+ * 제품 가져오기
+ * @return {Promise<Product[]>}
+ */
+export async function getProducts(): Promise<Product[]> {
+  try {
+    const q = query(collection(db, 'products'));
+
+    const querySnapshot = await getDocs(q);
+    const products: Product[] = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        productName: data.productName ?? '',
+        productBrand: data.productBrand ?? '',
+        productThumbnail: data.productThumbnail ?? '',
+      } as Product;
+    });
+
+    return products;
+  } catch (e) {
+    return [];
   }
 }
