33c6c77710532d0b590c714f940cd26012f1f8c0
Sumin Park <clappingmin@gmail.com>
2025-01-22 03:43:23 +0900

    feat: dearCard 리스트에서 조회데이터 연결

--- apps/asterum_traveler-app/src/components/dear/Card.tsx
+++ apps/asterum_traveler-app/src/components/dear/Card.tsx
@@ -9,38 +9,37 @@ import img_card_blue from '../../assets/images/report/card_blue.png';
 import img_card_yellowgreen from '../../assets/images/report/card_yellowgreen.png';
 import { Modal, ModalContent, ModalOverlay, useDisclosure } from '@chakra-ui/react';
 import ModalLetterDetail from './ModalLetterDetail';
-import { CardCoverColor } from '@asterum/types';
+import { CardCoverColor, DearCard } from '@asterum/types';
+import { timestampToDisplayDate } from '../../shared/utils';
 
-function Card() {
-  const TEMP_COLOR: CardCoverColor = 'red';
+const CARD_IMAGES = {
+  pink: img_card_pink,
+  red: img_card_red,
+  orange: img_card_orange,
+  yellow: img_card_yellow,
+  green: img_card_green,
+  skyblue: img_card_skyblue,
+  blue: img_card_blue,
+  yellowgreen: img_card_yellowgreen,
+};
 
-  const CARD_IMAGES = {
-    pink: img_card_pink,
-    red: img_card_red,
-    orange: img_card_orange,
-    yellow: img_card_yellow,
-    green: img_card_green,
-    skyblue: img_card_skyblue,
-    blue: img_card_blue,
-    yellowgreen: img_card_yellowgreen,
-  };
+interface CardProps {
+  dearCard: DearCard;
+}
+
+function Card({ dearCard }: CardProps) {
+  const { from, content, createdAt, cardCoverColor } = dearCard;
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
       <Wrapper>
-        <Cover coverColor="red" src={CARD_IMAGES[TEMP_COLOR]} />
+        <Cover coverColor={cardCoverColor} src={CARD_IMAGES[cardCoverColor]} />
         <MessageContainer onClick={onOpen}>
-          <CardTitle>작성자이름 10자까지</CardTitle>
-          <CardDate>2024.11.18</CardDate>
-          <CardContent>
-            우리는 이 곳에서 서로에게 영감을 주고, 플레이브에 대한 감사와 사랑을 공유합니다. 이
-            페이지는 추상적이고 아름다운 이야기의 집이며, 각자가 그 의미를 만들어갈 수 있는
-            공간입니다. 플레이브와 함께하는 모든 순간은 우리에게 더 큰 의미를 부여하고, 그들의
-            음악은 우리에게 특별한 여행을 선사합니다.함께 이 페이지에서 만나고, 플레이브와 함께하는
-            여정에서 새로운 감동과 기억을 만들어 나가요. 감사합니다, 빛나는 여러분! 편지 내용입니다
-          </CardContent>
+          <CardTitle>{from}</CardTitle>
+          <CardDate>{timestampToDisplayDate(createdAt)}</CardDate>
+          <CardContent>{content}</CardContent>
         </MessageContainer>
       </Wrapper>
       <Modal isOpen={isOpen} onClose={onClose} isCentered closeOnOverlayClick={true}>
--- apps/asterum_traveler-app/src/shared/services/dearService.ts
+++ apps/asterum_traveler-app/src/shared/services/dearService.ts
@@ -1,5 +1,5 @@
 import { DearCard, DearCardBase } from '@asterum/types';
-import { doc, setDoc, Timestamp } from 'firebase/firestore';
+import { collection, doc, getDocs, query, setDoc, Timestamp } from 'firebase/firestore';
 import { v4 as uuidv4 } from 'uuid';
 import { db } from '../firebaseConfig';
 
@@ -18,6 +18,33 @@ export async function addDearCard(dearCard: DearCardBase): Promise<string> {
       ...dearCard,
     } as DearCard);
     return cardId;
+  } catch (e) {
+    throw e;
+  }
+}
+
+/**
+ * 디어 카드 리스트 가져오기
+ * @return {Promise<DearCard[]>}
+ */
+export async function getDearCards(): Promise<DearCard[]> {
+  try {
+    const q = query(collection(db, 'cards'));
+
+    const querySnapshot = await getDocs(q);
+    const dearCards: DearCard[] = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: data.id,
+        createdAt: data.createdAt,
+        from: data.from ?? '',
+        password: data.password ?? '',
+        content: data.content ?? '',
+        cardCoverColor: data.cardCoverColor ?? 'pink',
+      } as DearCard;
+    });
+
+    return dearCards;
   } catch (e) {
     throw e;
   }
