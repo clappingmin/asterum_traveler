801b78e2d6862fa8e81b1d23ae4128e1e38cc042
Sumin Park <clappingmin@gmail.com>
2025-01-22 04:21:51 +0900

    docs: 할 일 추가

--- packages/types/interfaces/report.interface.ts
+++ packages/types/interfaces/report.interface.ts
@@ -32,7 +32,7 @@ export interface ReportBase {
   reportMembers: Member[];
   reportThumbnail: string;
   includedProducts: IncludedProduct[];
-  reportDate: { display: string; usage: string };
+  reportDate: { display: string; usage: string }; // TODO: usage Timestamp로 변경하고 정렬 기능 추가
   liveTitle?: string; // 라이브 리포트에서만 사용
   imageTags?: string[];
   reportUrl?: string; // 바로가기 연결할 주소
