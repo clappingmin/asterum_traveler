9f7529cbefc15dedf37acd7a788d133c2a4d19df
Sumin Park <clappingmin@gmail.com>
2025-02-25 18:45:17 +0900

    feat: 푸터 외부 주소 연결

--- apps/asterum_traveler-app/src/components/global/Footer.tsx
+++ apps/asterum_traveler-app/src/components/global/Footer.tsx
@@ -33,11 +33,21 @@ function Footer() {
             </InfoBox>
           </DeveloperBox>
           <SocialBox>
-            <SocialIcon src={Vlast} height={24} />
-            <SocialIcon src={X} height={24} />
-            <SocialIcon src={Youtube} height={24} />
-            <SocialIcon src={DaumCafe} height={24} />
-            <SocialIcon src={Weverse} height={24} />
+            <a href="https://vlast.com/" target="_blank">
+              <SocialIcon src={Vlast} height={24} />
+            </a>
+            <a href="https://x.com/plave_official" target="_blank">
+              <SocialIcon src={X} height={24} />
+            </a>
+            <a href="https://www.youtube.com/@plave_official" target="_blank">
+              <SocialIcon src={Youtube} height={24} />
+            </a>
+            <a href="https://cafe.daum.net/plave" target="_blank">
+              <SocialIcon src={DaumCafe} height={24} />
+            </a>
+            <a href="https://weverse.io/plave" target="_blank">
+              <SocialIcon src={Weverse} height={24} />
+            </a>
           </SocialBox>
         </ContactBox>
       </Container>
