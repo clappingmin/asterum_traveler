2000b140bd3b39938d11e6faa08d30b5a7b2c8f1
Sumin Park <clappingmin@gmail.com>
2025-03-10 18:14:52 +0900

    fix: test할 때 main.tsx 호출되는 오류 수정

--- apps/asterum_traveler-app/jest.config.ts
+++ apps/asterum_traveler-app/jest.config.ts
@@ -3,7 +3,8 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+    '^reset-css$': '<rootDir>/__mocks__/styleMock.js',
     '^styled-components$': require.resolve('styled-components'),
   },
   globals: {
