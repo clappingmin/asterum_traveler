0f64be258f8bb86f82d524710bf758e9067383b5
Sumin Park <clappingmin@gmail.com>
2025-03-18 00:00:26 +0900

    fix: ts를 js로 변경

--- apps/asterum_traveler-app/api/index.js
+++ apps/asterum_traveler-app/api/index.js
@@ -1,8 +1,7 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
 import { renderPage } from 'vite-plugin-ssr/server';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const pageContext = await renderPage({ urlOriginal: req.url! });
+export default async function handler(req, res) {
+  const pageContext = await renderPage({ urlOriginal: req.url });
 
   const { httpResponse } = pageContext;
   if (!httpResponse) {
