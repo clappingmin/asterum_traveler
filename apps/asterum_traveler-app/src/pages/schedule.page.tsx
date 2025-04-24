47808d94d8e1b3784a5f7b3f66593fda3a2cfc87
Sumin Park <clappingmin@gmail.com>
2025-04-24 17:12:31 +0900

    refector:  메모이제이션 코드 추가

--- apps/asterum_traveler-app/src/pages/schedule.page.tsx
+++ apps/asterum_traveler-app/src/pages/schedule.page.tsx
@@ -10,7 +10,7 @@ const YEAR = TODAY.getFullYear();
 
 function Page() {
   const [selectedMonth, setSelectedMonth] = useState(TODAY.getMonth() + 1);
-  const [refetchFn, setRefetchFn] = useState<(() => Promise<any>) | null>(null);
+  const [refetchFn, setRefetchFn] = useState<(() => Promise<unknown>) | null>(null);
 
   return (
     <>
